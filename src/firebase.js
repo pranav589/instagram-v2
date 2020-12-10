702f690930106a858707b0474c73235a5a16602e
Pranav Bakale <pranav.bakale@gmail.com>
2020-12-10 18:06:44 +0530

    fixes

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase'
 
 const firebaseApp =firebase.initializeApp( {
-    apiKey: "AIzaSyA0PA5WuzSYWM3KxIAIULkqveiaG6_aD8M",
-    authDomain: "instagram-910e2.firebaseapp.com",
-    databaseURL: "https://instagram-910e2.firebaseio.com",
-    projectId: "instagram-910e2",
-    storageBucket: "instagram-910e2.appspot.com",
-    messagingSenderId: "1044803520202",
-    appId: "1:1044803520202:web:c6b0074556e39daf035633",
-    measurementId: "G-SGW4CBYYWX"
+    //config here
   });
 
   const db=firebaseApp.firestore()
